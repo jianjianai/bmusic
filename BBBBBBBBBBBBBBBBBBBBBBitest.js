e8a1b766094cf0272e4abb300a376ba54624ae2d
jjaw <jjaw@jjaw.cn>
2024-09-09 19:03:11 +0800

    test

---
+++ BBBBBBBBBBBBBBBBBBBBBBitest.js
@@ -0,0 +1,33 @@
+
+
+/***
+ * 控制播放器进度 0~1 之间的小数
+ */
+function setPlaybackProgress(progress){
+    const line = document.querySelector('.bpx-player-progress-wrap');
+
+    const xAdd = line.getBoundingClientRect().left;
+    const width = line.getBoundingClientRect().width;
+    const pr =  width*progress;
+    const x = pr + xAdd;
+
+    line.dispatchEvent(new MouseEvent('mousedown', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: 0
+    }));
+    
+    window.dispatchEvent(new MouseEvent('mouseup', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        clientX: x,
+        clientY: 0
+    }));
+}
+
+
+
+
