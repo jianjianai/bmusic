5e45a794fe5b2bb4f5ca3b3861552926c331249c
jjaw <jjaw@jjaw.cn>
2024-10-24 17:24:25 +0800

    播放列表样式ok

--- src/renderer/src/components/PlayList.vue
+++ src/renderer/src/components/PlayList.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
 import { playListOpen } from '@renderer/states/playListState';
+import PlaySvg from './svg/Play.vue';
+import PauseSvg from './svg/Pause.vue';
 const test = [0];
 for (let i = 0; i < 100; i++) {
     test.push(i);
@@ -20,9 +22,19 @@ for (let i = 0; i < 100; i++) {
             <div class="list">
                 <!-- 列表中的每个项 -->
                 <div class="item-box" v-for="a of test">
+                    <!-- 图标 -->
+                    <div class="icon">
+                        <!-- TODO 图片显示 -->
+                        <!-- hover -->
+                        <div class="item-icon-hover"></div>
+                        <!-- 播放图标 -->
+                        <PlaySvg class="item-icon-svg"></PlaySvg>
+                        <!-- 暂停图标 -->
+                        <PauseSvg class="item-icon-svg"></PauseSvg>
+                    </div>
                     <div class="name-and-author">
-                        <div class="name">音乐名称adadadasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</div>
-                        <div class="author">音乐作者adadadasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd</div>
+                        <div class="name">音乐名称</div>
+                        <div class="author">音乐作者</div>
                     </div>
                     <div class="item">1:29</div>
                 </div>
@@ -31,19 +43,54 @@ for (let i = 0; i < 100; i++) {
     </div>
 </template>
 <style scoped>
-.item{
+.item-box:hover .item-icon-hover,
+.item-box:hover .item-icon-svg{
+    display: block;
+}
+.item-icon-svg{
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 1.5rem;
+    height: 1.5rem;
+    color: var(--color-play-list-item-icon-svg);
+    display: none;
+}
+.item-icon-hover{
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: var(--color-play-list-item-icon-hover);
+    display: none;
+}
+.icon {
+    width: 3rem;
+    height: 3rem;
+    background-color: var(--color-play-list-item-icon);
+    margin-right: 0.5rem;
+    border-radius: 0.5rem;
+    overflow: hidden;
+    position: relative;
+}
+
+.item {
     margin-left: 0.5rem;
     font-size: 0.8rem;
     color: var(--color-play-list-item-time);
 }
-.author{
+
+.author {
     color: var(--color-play-list-item-author);
     font-size: 0.8rem;
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
 }
-.name{
+
+.name {
     color: var(--color-play-list-item-name);
     font-size: 1rem;
     text-wrap: nowrap;
@@ -51,6 +98,7 @@ for (let i = 0; i < 100; i++) {
     text-overflow: ellipsis;
     white-space: nowrap;
 }
+
 .name-and-author {
     flex: 1;
     width: 0;
@@ -58,13 +106,16 @@ for (let i = 0; i < 100; i++) {
     flex-direction: column;
     gap: 0.2rem;
 }
+
 .item-box:hover {
     background-color: var(--color-play-list-item-hover);
 }
+
 .item-box {
     display: flex;
     align-items: center;
-    padding: 0.2rem 0.5rem 0.2rem 1rem;
+    padding: 0.5rem 0.5rem 0.5rem 1rem;
+    cursor: pointer;
 }
 
 .list {
@@ -75,7 +126,7 @@ for (let i = 0; i < 100; i++) {
     overflow-y: scroll;
     overflow-x: hidden;
     scrollbar-width: thin;
-    gap: 0.5rem;
+    /* gap: 0.5rem; */
 }
 
 .title-left {
@@ -109,7 +160,7 @@ for (let i = 0; i < 100; i++) {
     flex-direction: column;
     position: fixed;
     right: -30rem;
-    width: 22rem;
+    width: 23rem;
     top: 2rem;
     height: calc(100vh - 8rem);
     background-color: var(--color-play-list-window-bg);
