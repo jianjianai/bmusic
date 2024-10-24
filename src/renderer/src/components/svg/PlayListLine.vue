3cfde7ccea59efad149251b6eb2f128b5b9a4de9
jjaw <jjaw@jjaw.cn>
2024-10-25 00:12:25 +0800

    列表图标优化

--- src/renderer/src/components/svg/PlayListLine.vue
+++ src/renderer/src/components/svg/PlayListLine.vue
@@ -1,7 +1,7 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink"
         xml:space="preserve" width="100%" height="100%">
-        <path xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-            d="M44 36v4H4v-4zM4 7l16 10L4 27zm40 15v4H24v-4zM8 14.216v5.568L12.452 17zM44 8v4H24V8z"></path>
+        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M42 12H6m36 12H18m24 12H6m4-12-4-3v6Z"></path>
     </svg>
 </template>
