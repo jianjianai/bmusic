fd3a2f1f7293a0d8d2d92e9bf2a7e72dde357805
jjaw <jjaw@jjaw.cn>
2024-10-26 01:09:11 +0800

    测试音乐加歌图片好看点

--- src/renderer/src/components/musicPlayers/testMusicPlayer/TestMusicPlayer.vue
+++ src/renderer/src/components/musicPlayers/testMusicPlayer/TestMusicPlayer.vue
@@ -39,13 +39,15 @@ function testPayList() {
             musicName: "bilibili测试音乐1",
             musicAuthor: "测试作者",
             playerName: "BilibiliMusicPlayer",
-            playerData: `{"bvId":"BV19G411H7DY"}`
+            playerData: `{"bvId":"BV19G411H7DY"}`,
+            iconUrl: "file:///C:/Users/28018/Pictures/20181127140215_hvhdh.jpg"
         },
         {
             musicName: "bilibili测试音乐2",
             musicAuthor: "测试作者",
             playerName: "BilibiliMusicPlayer",
-            playerData: `{"bvId":"BV1eU4y1X7uA"}`
+            playerData: `{"bvId":"BV1eU4y1X7uA"}`,
+            iconUrl:"file:///C:/Users/28018/Pictures/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20240317073626.jpg"
         }
     ]);
     playList.setCurrentIndex(0); // 设置当前播放的音乐索引
