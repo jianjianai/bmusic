78044fc0c53dc607b21ed0e1940deb7cce73c5b2
jjaw <jjaw@jjaw.cn>
2024-11-08 19:52:34 +0800

    将进度条组件单独出来

--- src/renderer/src/components/MusicPlayerContr.vue
+++ src/renderer/src/components/MusicPlayerContr.vue
@@ -1,6 +1,5 @@
 <!-- 一个音乐播放控制组件，控制音乐播放暂停进度条，上一首下一首，播放模式，等.. -->
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
 import { musicPlayer } from '../states/musicPlayerStates'
 import ControllerSVG from '@renderer/svg/Controller.vue'
 import PauseSVG from '@renderer/svg/Pause.vue'
@@ -12,48 +11,7 @@ import SequentialPlaySvg from '@renderer/svg/SequentialPlay.vue'
 import ShufflePlaySvg from '@renderer/svg/ShufflePlay.vue'
 import { playList } from '@renderer/states/playListState'
 import FavoriteButton from './allSmall/FavoriteButton.vue'
-
-/**当前音乐播放到的百分比 0~1 之间的数字用于显示 仅当拖动进度条时用于预览显示 */
-const dragCurrentPercentage = ref(0.2);
-/** 真实播放进度百分比 */
-const currentPercentage = computed(() => musicPlayer.currentTime / Math.max(1, musicPlayer.duration));
-const lineCilckEl = ref();
-
-/** 拖动进度条 */
-const isMosueDown = ref(false);
-/** 用于当前显示的进度条百分比 */
-const shwoCurrentPercentage = computed(() => isMosueDown.value ? dragCurrentPercentage.value : currentPercentage.value);
-
-/**
- * 计算出应该跳转到的时间并更新
- * */
-function updateCurrentTime() {
-    musicPlayer.requestCurrentTime(musicPlayer.duration * dragCurrentPercentage.value);
-}
-
-function updateCurrentPercentage(event: MouseEvent) {
-    const rect = (lineCilckEl.value as HTMLElement).getBoundingClientRect();
-    const x = event.clientX - rect.left;
-    const width = rect.width;
-    dragCurrentPercentage.value = Math.min(1, Math.max(0, x / width));
-}
-function mosueMove(event: MouseEvent) {
-    updateCurrentPercentage(event);
-}
-function mosueUp(event: MouseEvent) {
-    isMosueDown.value = false;
-    window.removeEventListener('mousemove', mosueMove);
-    window.removeEventListener('mouseup', mosueUp);
-    updateCurrentPercentage(event);
-    updateCurrentTime();
-}
-function mosueDown(event: MouseEvent) {
-    isMosueDown.value = true;
-    window.addEventListener('mousemove', mosueMove);
-    window.addEventListener('mouseup', mosueUp);
-    updateCurrentPercentage(event);
-}
-
+import ControlLine from './allSmall/ControlLine.vue';
 
 /** 将毫秒为时间的单位转换为 mm:ss 的形式 */
 function formatTime(time: number) {
@@ -109,26 +67,28 @@ function formatTime(time: number) {
         <div class="control-line">
             <!-- 左边的时间 -->
             <div class="line-current">{{ formatTime(musicPlayer.currentTime) }}</div>
-            <div class="line-cilck" :class="{ mosueuse: isMosueDown }" @mousedown="mosueDown" ref="lineCilckEl">
-                <div class="line-box">
-                    <!-- 完整进度条 -->
-                    <div class="line-box-duration"></div>
-                    <!-- 播放到的 -->
-                    <div class="line-box-current"></div>
-                    <!-- 中间的按钮 -->
-                    <div class="line-box-button"></div>
-                </div>
-            </div>
+            <ControlLine class="line-inner" :currentTime="musicPlayer.currentTime" :duration="musicPlayer.duration"
+                :requestCurrentTime="musicPlayer.requestCurrentTime"></ControlLine>
             <!-- 右边的时间 -->
             <div class="line-duration">{{ formatTime(musicPlayer.duration) }}</div>
         </div>
     </div>
 </template>
 <style scoped>
+.line-inner{
+    margin: 0 0.5rem;
+    width: 20rem;
+}
+
 .play-mode-btn {
     width: 1.5rem;
     height: 1.5rem;
     cursor: pointer;
+    color: var(--color-contr-play-mode-btn);
+}
+
+.play-mode-btn:hover {
+    color: var(--color-contr-play-mode-btn-hover);
 }
 
 .play-mode-icon {
@@ -216,58 +176,6 @@ function formatTime(time: number) {
 .line-duration {
     font-size: 0.6rem;
     color: var(--color-contr-line-text);
-}
-
-.line-cilck:hover .line-box-button,
-.line-cilck.mosueuse .line-box-button {
-    opacity: 1;
-}
-
-.line-box-button {
-    opacity: 0;
-    position: absolute;
-    left: v-bind("`${shwoCurrentPercentage * 100}%`");
-    top: 50%;
-    transform: translate(-50%, -50%);
-    width: 0.8rem;
-    height: 0.8rem;
-    border-radius: 0.8rem;
-    background-color: var(--color-contr-line-btn-bg);
-    box-shadow: 0rem 0.1rem 0.3rem var(--color-contr-line-btn-shadow)
-}
-
-.line-box-current {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 100%;
-    width: v-bind("`${shwoCurrentPercentage * 100}%`");
-    background-color: var(--color-contr-line-current-bg);
-    border-radius: 1rem;
-}
-
-.line-box-duration {
-    position: absolute;
-    left: 0;
-    top: 0;
-    height: 100%;
-    width: 100%;
-    background-color: var(--color-contr-line-bg);
-    border-radius: 1rem;
-}
-
-.line-box {
-    width: 20rem;
-    height: 0.25rem;
-    position: relative;
-}
-
-.line-cilck {
-    height: 0.5rem;
-    margin: 0 0.5rem;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
 }
 
 .control-line {
